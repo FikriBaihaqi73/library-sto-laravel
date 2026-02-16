761724a9fa5ee46e485eefb3b714e9ee6ecc7110
FikriBaihaqi73 <fikrimuhammad38993@gmail.com>
2026-02-16 08:21:45 +0700

    fix: include vendor and add better error reporting

--- api/index.php
+++ api/index.php
@@ -4,14 +4,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if (!extension_loaded('sodium')) {
-    // echo "Extension 'sodium' is not loaded!";
-}
-
 if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    echo "Autoloader not found! Did composer install run?";
+    echo "<h1>Eror: Autoloader (vendor) tidak ditemukan!</h1>";
+    echo "<p>Coba jalankan 'composer install' secara lokal lalu push folder vendor-nya, atau pastikan Vercel menjalankan composer install.</p>";
     exit;
 }
-
 
 require __DIR__ . '/../public/index.php';
